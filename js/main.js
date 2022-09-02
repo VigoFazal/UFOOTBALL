0f59f2ab72b1af8d9d69234758024c47a608b645
VigoFazal <110392931+VigoFazal@users.noreply.github.com>
2022-09-02 04:51:32 -0700

    v__0.1

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// TIMER 
+$(function(){
+	FlipClock.Lang.Custom = { days:'DAYS', hours:'HOURS', minutes:'MINUTES', seconds:'SECONDES' };
+	var opts = {
+		clockFace: 'DailyCounter',
+		countdown: true,
+		language: 'Custom'
+	};
+	opts.classes = {
+		active: 'flip-clock-active',
+		before: 'flip-clock-before',
+		divider: 'flip-clock-divider',
+		dot: 'flip-clock-dot',
+		label: 'flip-clock-label',
+		flip: 'flip',
+		play: 'play',
+		wrapper: 'flip-clock-small-wrapper'
+	};  
+	var countdown = 1662906900 - ((new Date().getTime())/1000); // from: 09/11/2022 07:35 am -0700
+	countdown = Math.max(1, countdown);
+	$('.clock-builder-output').FlipClock(countdown, opts);
+});
+
+
+// HEADER SLIDER PHOTO SLIDER 
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 6,
+	spaceBetween: 0,
+	loop: true,
+	centeredSlides: true,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+});
+
+// var swiper = new Swiper(".mySwiper-1", {
+// 	slidesPerView: 1,
+// 	spaceBetween: 0,
+// 	loop: true,
+// 	centeredSlides: true,
+// 	navigation: {
+// 	  nextEl: ".swiper-button-next",
+// 	  prevEl: ".swiper-button-prev",
+// 	},
+// });
+var swiper2 = new Swiper(".mySwiper-1", {
+	slidesPerView: 1,
+	grabCursor: true,
+	effect: "creative",
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+	spaceBetween: 10,
+	loop: true,
+	creativeEffect: {
+	  prev: {
+		shadow: true,
+		translate: ["-120%", 0, -500],
+	  },
+	  next: {
+		shadow: true,
+		translate: ["120%", 0, -500],
+	  },
+	},
+  });
